aca0645f711fd791de5f7919ab96f3d50abc335b
BenniKenobi <Bennedict.Schweimer@haw-hamburg.de>
2021-12-21 15:04:14 +0100

    Update Nutzer.java

--- app/src/main/java/com/example/myapplication/Nutzer.java
+++ app/src/main/java/com/example/myapplication/Nutzer.java
@@ -1,4 +1,54 @@
 package com.example.myapplication;
 
 public class Nutzer {
+
+    private String passwort;
+    public String benutzername, email, telefonnummer, ort, postleitzahl, bio;
+
+    public Nutzer(String benutzername, String passwort, String email, String telefonnummer, String ort, String postleitzahl){
+        benutzername = this.benutzername;
+        passwort = this.passwort;
+        email = this.email;
+        telefonnummer = this.telefonnummer;
+        ort = this.ort;
+        postleitzahl = this.postleitzahl;
+    }
+
+    public void schreibeDB(){
+        //todo
+    }
+
+    public void ladeDB(){
+        //todo
+    }
+
+    public void bearbeiten(String cmd, String data){
+        switch(cmd){
+            case "benutzername":
+                this.benutzername = data;
+                //todo schreibeDB()
+                break;
+            case "passwort":
+                this.passwort = data;
+                //todo schreibeDB()
+                break;
+            case "email":
+                this.email = data;
+                //todo schreibeDB()
+                break;
+            case "telefonnummer":
+                this.telefonnummer = data;
+                //todo schreibeDB()
+                break;
+            case "ort":
+                this.ort = data;
+                //todo schreibeDB()
+                break;
+            case "postleitzahl":
+                this.benutzername = data;
+                //todo schreibeDB()
+                break;
+        }
+
+    }
 }
