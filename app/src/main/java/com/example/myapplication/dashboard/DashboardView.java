5fa3c7d5116963ec54c24978a50df0cc7d93f8de
Liano22 <82031654+Liano22@users.noreply.github.com>
2022-01-19 19:02:24 +0100

    dashboard anfang

--- app/src/main/java/com/example/myapplication/dashboard/DashboardView.java
+++ app/src/main/java/com/example/myapplication/dashboard/DashboardView.java
@@ -1,4 +1,25 @@
 package com.example.myapplication.dashboard;
 
-public class DashboardView implements DashboardContract.View{
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.ScrollView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DashboardView extends AppCompatActivity implements DashboardContract.View{
+
+    Button addDogBtn;
+
+    RecyclerView recyclerViewDashboard;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        
+
+    }
 }
