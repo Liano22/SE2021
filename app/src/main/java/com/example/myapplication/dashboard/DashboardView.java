d07f8c619bbae183e89085a09ca09bd202383ece
Liano22 <82031654+Liano22@users.noreply.github.com>
2022-01-21 10:24:32 +0100

    Update DashboardView.java

--- app/src/main/java/com/example/myapplication/dashboard/DashboardView.java
+++ app/src/main/java/com/example/myapplication/dashboard/DashboardView.java
@@ -2,12 +2,15 @@ package com.example.myapplication.dashboard;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ScrollView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
 
 public class DashboardView extends AppCompatActivity implements DashboardContract.View{
 
@@ -19,7 +22,17 @@ public class DashboardView extends AppCompatActivity implements DashboardContrac
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
 
-        
+        setContentView(R.layout.dashboard_activity);
+
+        addDogBtn = findViewById(R.id.addDogBtn);
+        recyclerViewDashboard = findViewById(R.id.recyclerViewDashboard);
+
+        addDogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //add new Dog
+            }
+        });
 
     }
 }
