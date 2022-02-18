c6ae853eab9cb43e62b96e9da5e748160b0211a5
Viktyny <ralf.herrmann@haw-hamburg.de>
2022-02-18 15:12:32 +0100

    Kleine Anpassungen am Interface
    Kleine Anpassungen am Interface und mit Klassen die damit zusammenhängen im Package Filter

--- app/src/main/java/com/example/myapplication/filter/IFilterContract.java
+++ app/src/main/java/com/example/myapplication/filter/IFilterContract.java
@@ -1,18 +1,22 @@
 package com.example.myapplication.filter;
 
+import com.google.firebase.database.Query;
+
 public interface IFilterContract {
     //Karl & Ralf
 
     interface IView{
+        boolean checkRadioButton();
 
     }
 
     interface IPresenter{
-        void filter(String race, String age, String minPrice, String maxPrice, boolean papersAvailable);
+        //void filter(String race, String age, String minPrice, String maxPrice, boolean papersAvailable);
 
+        boolean validateFields(String minprice, String maxprice);
     }
 
     interface IModel{
-
+        Query getAllDogs();
     }
 }
